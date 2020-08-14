fc18296701e70f944efd0edfe8a1c0e6bc4d9b19
Scott Meyers <scott@siberia.io>
2020-08-14 12:48:23 -0400

    updates activities

---
+++ web/src/App/index.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
+import * as config from 'config';
+import activities from 'activities.json';
+
+const App: React.FC = () => {
+  const [map, setMap] = useState();
+  const mapOptions = useRef<google.maps.MapOptions>({
+    center: { lat: 40.699, lng: -73.976 },
+    clickableIcons: false,
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    draggableCursor: 'arrow',
+    minZoom: 10,
+    maxZoom: 16,
+    panControl: false,
+    scrollwheel: true,
+    styles: config.mapStyles,
+    zoom: 14,
+  });
+
+  useEffect(() => {
+    if (!map) return;
+    activities.map((route) => map.data.addGeoJson(route));
+    map.data.setStyle({
+      strokeColor: 'red',
+      strokeWidth: 1,
+    });
+  }, [map]);
+
+  return (
+    <LoadScript
+      googleMapsApiKey={config.apiKey}
+      libraries={config.mapLibraries}
+      loadingElement={<></>}
+    >
+      <GoogleMap id="map" options={mapOptions.current} onLoad={setMap} />
+    </LoadScript>
+  );
+};
+
+export default App;
---
+++ web/src/config.ts
@@ -0,0 +1,141 @@
+export const apiKey = process.env.REACT_APP_GMAPS_API_KEY;
+
+export const mapLibraries = ['places', 'drawing', 'geometry'];
+
+export const mapStyles: google.maps.MapTypeStyle[] = [
+  {
+    featureType: 'administrative',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#444444',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'all',
+    stylers: [
+      {
+        color: '#000000',
+      },
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'all',
+    stylers: [
+      {
+        saturation: -100,
+      },
+      {
+        lightness: 45,
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#eaeaea',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#dedede',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [
+      {
+        color: '#434343',
+      },
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+];
